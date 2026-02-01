a7347afb90af8128127aab14797e50ad8e6845bf
Adrian Ologu <148220843+sito2004@users.noreply.github.com>
2026-02-01 19:58:32 +0100

    Update Servidor.java

--- TCP/Servidor.java
+++ TCP/Servidor.java
@@ -6,8 +6,6 @@ import java.util.*;
 
 public class Servidor {
 
-    // Lista compartida para guardar los flujos de salida de todos los clientes
-    // Usamos Collections.synchronizedList para seguridad básica en hilos
     private static List<PrintWriter> clientesConectados = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) throws IOException {
@@ -15,12 +13,12 @@ public class Servidor {
         System.out.println("Servidor TCP iniciado en puerto 5000...");
 
         while (true) {
-            // 1. Esperar conexión
+
             Socket socketCliente = servidor.accept();
 
-            // 2. Crear y arrancar el Handler pasando el socket y la lista compartida
             ClienteHandler handler = new ClienteHandler(socketCliente, clientesConectados);
             handler.start();
         }
     }
+
 }
